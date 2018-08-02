67a2d9b820c8047e254cc7af0d571bbbdfd974b1
iarangor1 <41337664+iarangor1@users.noreply.github.com>
2018-08-01 19:04:54 -0500

    Add files via upload

---
+++ talleres/taller03/PuntosOpcionales.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Jd here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+class PuntosOpcionales {
+    public static void Punto3(String str) {              
+        int r = str.length();
+        int n = r;   
+        Punto3Aux(str, "", n, r);
+    }
+
+    private static void Punto3Aux(String str, String base, int n, int r) {
+        if (n == 0) {
+            System.out.print(base + " ");
+        } else {
+            for (int i = 0; i < r; i++) {
+                //Para el ejercicio 3 quita el if
+                Punto3Aux(str, base + str.charAt(i), n-1, r);
+
+            }
+        }
+    }
+}

57a427d378a2840af02211cd8c0bc47ebfb03d0c
iarangor1 <41337664+iarangor1@users.noreply.github.com>
2018-09-23 12:48:51 -0500

    Add files via upload

--- proyecto/codigo/Project.java
+++ proyecto/codigo/Project.java
@@ -1,13 +1,27 @@
 import java.io.*;
 import java.util.*;
 
+/**
+
+ * Esta clase recibe las coordenadas de un grupo de abejas y retorna las que
+se encuentren a 100 metros o menos.
+
+ * @author: Isabella Arango & Juan David Rengifo Castro
+
+ * @version: 23/09/2018/A
+
+ */
 public class Project {
     private static final int RADIUS = 6371000;
     public static ArrayList<String> bees = new ArrayList<String>();
     private static ArrayList<Double> latitude = new ArrayList<Double>();
     private static ArrayList<Double> longitud = new ArrayList<Double>();
     private static ArrayList<Double> height = new ArrayList<Double>();
-
+    /** Método main que se encarga de leer el archivo .txt con las coordenadas de las abejas
+     *  y pasarlas a un ArrayList tipo String.
+     * @see #toDouble()
+     * @see #closeBees()
+     */
     public static void main (String[] args) throws FileNotFoundException {
         try{
             Scanner input = new Scanner(new File("coordenadas.txt"));
@@ -27,6 +41,11 @@ public class Project {
         }
     }
 
+    /**
+     * Método que pasa las coordenadas del ArrayList tipo String a tres ArrayList tipo double,
+     * donde se dividen las coordenadas en latitud, longitud y altura.
+     * @see #Main()
+     */
     public static void toDouble() {
         for (int i = 0; i<bees.size(); i++) {  
             String s = bees.get(i);
@@ -42,17 +61,38 @@ public class Project {
         }
     }
 
+    /**
+     * Mètodo que retorna la distancia entre dos coordenadas cardinales.
+     * @param lat: latitud de la coordenada 1.
+     * @param lat1: latitud de la coordenada 2.
+     * @param lon: longitud de la coordenada 1.
+     * @param lon1: longitud de la coordenada 2.
+     * @return La distancia en tres dos coordenadas cardinales.
+     * @see #closeBees().
+     */
     public static double Haversine(double lat, double lon, double lat1, double lon1) {
         double d = 2*RADIUS*Math.asin(Math.sqrt(Math.pow(Math.sin(Math.toRadians((lat1-lat)/2.0)),2)+
                     Math.cos(lat1)*Math.cos(lat)*Math.pow(Math.sin(Math.toRadians((lon1-lon)/2)),2)));
         return d;
     }
 
+    /**
+     * Mètodo que retorna la distancia pitagorica entre el diferencial de la altura y la distancia de dos coordenadas cardinales.
+     * @param dh: distancia entre dos coordenadas cardinales.
+     * @param heihgt: diferencial de la altura de dos abejas.
+     * @return distancia pitagòrica entre los parametros.
+     * @see #closeBees().
+     */
     public static double Pitagoras (double dh, double height) {
         double d = Math.sqrt(dh*dh + height*height);
         return d;
     }
-
+    
+    /**
+     * Mètodo que imprime las abejas que estan a 100 metros o menos y su respectiva distancia.
+     * @see #Pitagoras(dh, height).
+     * @see #Haversine(lat, lon, lat1, lon1)
+     */
     public static void closeBees (){
         for (int i = 0; i<bees.size()/3; i++){
             for (int j = 0; j<bees.size()/3; j++) {
